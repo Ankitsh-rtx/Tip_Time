f10febcb87cc503660aae2d153133bf26aff93fe
Ankit <ankitshaw1112@gmail.com>
2022-04-24 01:50:12 +0530

    themes and icon added

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -8,29 +8,33 @@ import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.result.text = getString(R.string.tip_amount,"0.00")
-        binding.calculate.setOnClickListener { calculateTip() }
+        binding.resultTextView.text = getString(R.string.tip_amount, "0.00")
+        binding.calculateBtn.setOnClickListener { calculateTip() }
     }
-    private fun calculateTip(){
-        val cost = binding.cost.text.toString()
-        val costInput = cost.toDouble()
-        val input = when(binding.radioGroup.checkedRadioButtonId){
-            R.id.radio_20-> 0.20
-            R.id.radio_18-> 0.18
-            else-> 0.15
+
+    private fun calculateTip() {
+        var cost = binding.costEditText.text.toString()
+        if (cost == "") {
+            cost = "0.00"
         }
-        var tip = (costInput*input)
-        if(binding.roundValue.isChecked){
+        val costInput = cost.toDouble()
+        val input = when (binding.radioGroup.checkedRadioButtonId) {
+            R.id.radio_btn_20 -> 0.20
+            R.id.radio_btn_18 -> 0.18
+            else -> 0.15
+        }
+        var tip = (costInput * input)
+        if (binding.roundUpSwitch.isChecked) {
             tip = ceil(tip)
         }
         val tipFormat = NumberFormat.getNumberInstance().format(tip)
-        binding.result.text = getString(R.string.tip_amount,tipFormat)
+        binding.resultTextView.text = getString(R.string.tip_amount, tipFormat)
 
     }
 }
