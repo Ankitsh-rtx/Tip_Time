4f033e986e347fd47b420d98430223049be09638
Ankit <ankitshaw1112@gmail.com>
2022-04-23 18:14:59 +0530

    provided app functioning by adding calculate tip method

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tiptime
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.result.text = getString(R.string.tip_amount,"0.00")
+        binding.calculate.setOnClickListener { calculateTip() }
+    }
+    private fun calculateTip(){
+        val cost = binding.cost.text.toString()
+        val costInput = cost.toDouble()
+        val input = when(binding.radioGroup.checkedRadioButtonId){
+            R.id.radio_20-> 0.20
+            R.id.radio_18-> 0.18
+            else-> 0.15
+        }
+        var tip = (costInput*input)
+        if(binding.roundValue.isChecked){
+            tip = ceil(tip)
+        }
+        val tipFormat = NumberFormat.getNumberInstance().format(tip)
+        binding.result.text = getString(R.string.tip_amount,tipFormat)
+
     }
 }
